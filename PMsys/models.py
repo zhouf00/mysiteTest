7caaf7cc1c49fe4562a0fe1e76d229ca2ba44867
zhouf <41719122@qq.com>
2020-02-17 09:55:01 +0800

    homework

--- PMsys/models.py
+++ PMsys/models.py
@@ -1,32 +1,40 @@
 from django.db import models
 
+from db.base_model import BaseModel
+
 # Create your models here.
 
 
-class Items(models.Model):
+class Items(BaseModel):
     """
     所有业主、主机厂、任务单管理、日志共有的数据表
     """
 
-    item_type_choice = (
+    ITEMS_TYPE_CHOICE = (
         ('wind', '风电'),
         ('fire', '火电'),
     )
 
-    item_status = (
+    ITEM_STATUS = (
         ('0', '新建'),
         ('1', '保修期内'),
         ('2', '过保修期'),
     )
 
-    item_type = models.CharField(choices=item_type_choice, max_length=64, default='wind', verbose_name='项目类型')
+    item_type = models.CharField(choices=ITEMS_TYPE_CHOICE, max_length=64, default='wind', verbose_name='项目类型')
     name = models.CharField(max_length=64, unique=True, verbose_name='项目名称')  # 不可重复
-    sn = models.CharField(max_length=128, unique=True, verbose_name='项目编号')  # 不可重复
-    status = models.CharField(choices=item_status, default=0, verbose_name='项目状态')
+    status = models.CharField(choices=ITEM_STATUS, default=0, verbose_name='项目状态')
+    address = models.CharField(max_length=64, null=True, blank=True, verbose_name='地址')
     manufacturer = models.ForeignKey('Manufacturer', null=True, blank=True, verbose_name='主机厂信息',
                                      on_delete=models.SET_NULL)
-    managerment = models.ForeignKey('Managerment', null=True, blank=True, verbose_name='业主信息',
+    image = models.ImageField(max_length=100, null=True, blank=True, verbose_name='默认图片')
+    managerfirm = models.ForeignKey('ManagerFirm', null=True, blank=True, verbose_name='业主信息',
                                      on_delete=models.SET_NULL)
+    entrancetime = models.DateTimeField(verbose_name='入场时间')
+    endtime = models.DateTimeField(verbose_name='完成时间')
+    accpettime = models.DateTimeField(verbose_name='验收时间')
+    facilitycount = models.IntegerField(verbose_name='采集器数量')
+    sensorcount = models.IntegerField(verbose_name='传感器数量')
 
     def __str__(self):
         return self.name
@@ -36,26 +44,28 @@ class Items(models.Model):
         verbose_name_plural = verbose_name
 
 
-class Managerment(models.Model):
+class ManagerFirm(BaseModel):
     """
-    业主信息
+    项目管理公司
     """
 
-    name = models.CharField(max_length=64, verbose_name='业主名称')
-    about = models.TextField(verbose_name='简介')
-    linkman = models.CharField(max_length=32, null=True, blank=True, verbose_name='业主联系人')
-    mobile = models.CharField(max_length=11, default='', verbose_name='业主联系电话')
-    memo = models.CharField(null=True, blank=True, verbose_name='备注')
+    name = models.CharField(max_length=64, verbose_name='管理公司名称')
+    memo = models.TextField(null=True, blank=True, verbose_name='备注')
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = '厂商'
+        verbose_name = '项目管理公司'
         verbose_name_plural = verbose_name
 
 
-class Manufacturer(models.Model):
+class ItemUsers(BaseModel):
+    """项目联系人表"""
+    pass
+
+
+class Manufacturer(BaseModel):
     """主机厂信息"""
 
     name = models.CharField(max_length=64, verbose_name='主机厂名称')
---
+++ db/base_model.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+class BaseModel(models.Model):
+    """模型类抽象基类"""
+
+    create_time = models.DateTimeField(auto_created=True, verbose_name='创建时间')
+    update_time = models.DateTimeField(auto_now=True, verbose_name='更新时间')
+    is_delete = models.BooleanField(default=False, verbose_name='删除标记')
+
+    class Meta:
+        # 说明
+        abstract = True
